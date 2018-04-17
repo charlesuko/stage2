82b5c3868bc8c2976e8f61bca9f7477be4b11e49
Charles Uko <charles4genius@yahoo.com>
2018-04-17 10:58:46 +0000

    added the remaining files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="hng.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:100,400" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:700" rel="stylesheet">
+
+</head>
+
+<body>
+	
+	<div id="quote">"The time is always right <br> to do the right thing"</div>
+
+	<div id="time">
+	<?php
+	date_default_timezone_set("Africa/Lagos");
+	echo date("h:i:sa");?>
+		
+	</div>
+
+	<div id="hng"><p>HNG INTERNSHIP 4.0</p></div>
+
+</body>
+</html>
+
+
